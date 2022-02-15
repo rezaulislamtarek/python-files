e29db2e28d098b172810ca61ae1e552024a9870c
tarek <mrezaulislam.t@gmail.com>
2022-02-15 22:04:00 +0600

    operation with getcwd, chdir and scandir

---
+++ os_one.py
@@ -0,0 +1,24 @@
+import os
+
+
+def display_cwd():
+    cwd = os.getcwd()
+    print(f"current working directory: {cwd}")
+
+
+def up_one_directory_level():
+    os.chdir("../")
+
+
+def display_entries_in_directory(directory):
+    print("------------------------Scandir Call-----------------------------")
+    with os.scandir(directory) as entries:
+        for entry in entries:
+            print(entry.name)
+
+
+if __name__ == "__main__":
+    display_cwd()
+    up_one_directory_level()
+    display_cwd()
+    display_entries_in_directory(os.getcwd()+"/PythonProject1")
